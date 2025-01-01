e84cc424fcf71c466f7aeec89ad464c56b1e277d
Beamlak Aschalew <birrletej12@gmail.com>
2025-01-01 08:32:40 +0300

    created landing page

--- views/index.view.php
+++ views/index.view.php
@@ -8,9 +8,23 @@
 </head>
 
 <body>
-    <?php require 'partials/navigation.php'; ?>
-    <h1>Hello</h1>
-    <?php require 'partials/footer.php'; ?>
+    <!-- <?php require 'partials/navigation.php'; ?> -->
+    <main class="main-container">
+        <div class="inside-wrapper">
+            <div class="text-container">
+                <h1>Online courses at your convinience</h1>
+                <p>Master real world skills and knowledge without any disruption at your comfort zone</p>
+                <div class="action-container">
+                    <a class="button login" href="">Login</a>
+                    <a class="button signup" href="">Signup</a>
+                </div>
+            </div>
+            <div class="image-container">
+                <img src="../assets/images/online_learning.png" alt="" srcset="">
+            </div>
+        </div>
+    </main>
+    <!-- <?php require 'partials/footer.php'; ?> -->
 </body>
 
 </html>
