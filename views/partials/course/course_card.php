3afa13ffafb31361991d2cf55c136f5d601c27d4
Beamlak Aschalew <birrletej12@gmail.com>
2025-01-18 22:38:26 +0300

    added link to course card

--- views/partials/course/course_card.php
+++ views/partials/course/course_card.php
@@ -1,17 +1,19 @@
-<div class="course">
-    <div class="course-image-container">
-        <img
-            class="course-image"
-            src="../../assets/images/c5.webp"
-            alt="" />
-    </div>
-    <div class="course-text">
-        <div class="course-title">
-            Web development
+<a href="/course/course-name">
+    <div class="course">
+        <div class="course-image-container">
+            <img
+                class="course-image"
+                src="../../assets/images/c5.webp"
+                alt="" />
         </div>
+        <div class="course-text">
+            <div class="course-title">
+                Web development
+            </div>
 
-        <div class="course-author">Dr. Angela Yu</div>
-        <div class="course-price">300birr</div>
-        <div class="course-category">Development</div>
+            <div class="course-author">Dr. Angela Yu</div>
+            <div class="course-price">300birr</div>
+            <div class="course-category">Development</div>
+        </div>
     </div>
-</div>
+</a>
