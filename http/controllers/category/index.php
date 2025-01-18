6833c9b0b18635f00220433ecebd460840c8e817
Beamlak Aschalew <birrletej12@gmail.com>
2025-01-18 22:32:58 +0300

    added remote connection

---
+++ http/controllers/category/index.php
@@ -0,0 +1,3 @@
+<?php
+
+view('category/index.view.php', []);
--- routes.php
+++ routes.php
@@ -5,8 +5,10 @@ $router->get('/', 'index.php');
 $router->get('/signup', 'auth/signup.php');
 $router->get('/login', 'auth/login.php');
 
-$router->get('/course/{course-slang}', 'course/detail.php');
+$router->get('/course/{course-slug}', 'course/detail.php');
 
 $router->post('/search/{search-term}', 'search/index.php');
 $router->get('/search/{search-term}', 'search/index.php');
 $router->get('/live/{query}', 'search/live.php');
+
+$router->get('/category/{category-slug}', 'category/index.php');
