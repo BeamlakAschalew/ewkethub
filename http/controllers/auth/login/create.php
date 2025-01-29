81af0432da19fb1a6119847a64796edd9088663e
Edlawit Solomon <solomonedlawit901@gmail.com>
2025-01-29 14:21:43 +0300

    updated login

--- http/controllers/auth/login/create.php
+++ http/controllers/auth/login/create.php
@@ -1,3 +1,33 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-dd('Login create');
+use Core\Database;
+
+$errors = [];
+$data = $_POST;
+$config = require base_path("essentials/config.php");
+$database = new Database($config["database"]);
+
+$student = $database->query("SELECT * FROM student WHERE email = :email OR username = :username", [
+    'email' => $data['emailUsername'],
+    'username' => $data['emailUsername'],
+])->find();
+
+if (!$student) {
+    $errors['noUser'] = 'No user found with your username';
+    view('auth/login/index.view.php', ['errors' => $errors]);
+    die();
+}
+
+if ($student) {
+    if (!password_verify($data['password'], $student['password'])) {
+        $errors['incorrectPassword'] = 'Incorrect password';
+        view('auth/login/index.view.php', ['errors' => $errors]);
+        die();
+    }
+}
+
+setcookie("student", json_encode($student), time() + (432000 * 30), "/");
+header("Location: /");
